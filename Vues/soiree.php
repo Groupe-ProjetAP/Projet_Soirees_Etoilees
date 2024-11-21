26b02fe43fc60e91ca50d1fa07a30bdfa170b2a4
Thomas SÉRÉ-LAPIQUE <thomassere.lapique@gmail.com>
2024-11-21 17:13:06 +0100

    modif

--- Vues/soiree.php
+++ Vues/soiree.php
@@ -31,16 +31,27 @@
                     }else{
                         echo "<td>Debout</td>";
                     }
-                echo "<td>" . $uneSoirée->getInfoComp() . "</td>";
+
+                // Info Complémentaire avec affichage limité et popup pour plus de texte
+                $infoComp = $uneSoirée->getInfoComp();
+                $maxLength = 10; // Limite de caractères affichés
+                if (strlen($infoComp) > $maxLength) {
+                    // Si le texte est plus long que la limite, on montre un extrait avec un lien
+                    echo "<td>" . substr($infoComp, 0, $maxLength) . '...';
+                    echo "<a href='#' class='show-more' data-info='".$infoComp."'> Voir plus</a></td>";
+                } else {
+                    // Si le texte est court, on l'affiche en entier
+                    echo "<td>" . $infoComp . "</td>";
+                }
+
                 echo "<td>";
                     echo "<a class='btn' href='index.php?controleur=Soiree&action=supprimerSoiree&idSoiree=" . $uneSoirée->getId() . "'>Supprimer</a>"."</br>"."</br>";
                     echo " ";
-
                     echo "<a class='btn' href='index.php?controleur=Soiree&action=modifierSoiree&nom=" . $uneSoirée->getNom() . "&nbPlace=" . $uneSoirée->getNbPlace() . "&lieu=".$uneSoirée->getLieu(). "&dateSoiree=" . $uneSoirée->getDateSoiree()."&idSoiree=".$uneSoirée->getId()."&heureSoiree=" . $uneSoirée->getHeureSoiree()."&placeAssise=" . $uneSoirée->getPlaceAssise()."&infoComp=" . $uneSoirée->getInfoComp() . "'>Modifier</a>";
-
                 echo "</td>";
             echo "</tr>";
         }
         ?>
+        <script src="./script/popup.js"></script>
     </tbody>
 </table>
