8c56e2c9eeaabfa0c9b8464ce9fbc9a67af47357
Thomas SÉRÉ-LAPIQUE <thomassere.lapique@gmail.com>
2024-11-28 15:15:09 +0100

    carrection SoireeDAO

--- Modeles/SoireeDAO.php
+++ Modeles/SoireeDAO.php
@@ -2,26 +2,37 @@
 include 'modeles/Base.php';
 include './Modeles/Soiree.php';
 
-class SoireeDAO extends Base{
+class SoireeDAO extends Base {
     
     public function __construct() {
-        parent::__construct('projetsjp','69844fqfqkmILJ5');
+        parent::__construct('projetsjp', '69844fqfqkmILJ5');
     }
-    
+
     /**
      * Récupère la liste de toutes les soirées depuis la base de données.
      * @return Soiree[]
      */
     public function getLesSoirees() {
-      
-        $resultatRequete= $this ->query("SELECT idSoiree, nom,nbPlaces,lieu,dateSoiree,heureSoiree,placeAssise,infoComp FROM soiree");
-        $tableauSoirée=$resultatRequete->fetchAll();
-        $listeSoirées=array();
-        foreach ($tableauSoirée as $uneLigneUneSoirée) {
-            $unObjetCompetence = new Soiree($uneLigneUneSoirée["idSoiree"], $uneLigneUneSoirée["nom"],$uneLigneUneSoirée['nbPlaces'],$uneLigneUneSoirée['lieu'],$uneLigneUneSoirée['dateSoiree'],$uneLigneUneSoirée['heureSoiree'],$uneLigneUneSoirée['placeAssise'],$uneLigneUneSoirée['infoComp']);
-            $listeSoirées[]=$unObjetCompetence;
+        $sql = "SELECT idSoiree, nom, nbPlaces, lieu, dateSoiree, heureSoiree, placeAssise, infoComp FROM soiree";
+        $stmt = $this->prepareAndExecute($sql);
+        $tableauSoiree = $stmt->fetchAll();
+        $listeSoirees = [];
+
+        foreach ($tableauSoiree as $uneLigneUneSoiree) {
+            $unObjetSoiree = new Soiree(
+                $uneLigneUneSoiree["idSoiree"],
+                $uneLigneUneSoiree["nom"],
+                $uneLigneUneSoiree['nbPlaces'],
+                $uneLigneUneSoiree['lieu'],
+                $uneLigneUneSoiree['dateSoiree'],
+                $uneLigneUneSoiree['heureSoiree'],
+                $uneLigneUneSoiree['placeAssise'],
+                $uneLigneUneSoiree['infoComp']
+            );
+            $listeSoirees[] = $unObjetSoiree;
         }
-        return $listeSoirées;
+
+        return $listeSoirees;
     }
 
     /**
@@ -30,48 +41,69 @@ class SoireeDAO extends Base{
      * @return bool|int
      */
     public function SupprimerSoiree($idSoiree) {
-        $resultatRequete= $this ->exec("DELETE FROM `soiree` WHERE idSoiree =$idSoiree");
-        return $resultatRequete;
+        $sql = "DELETE FROM soiree WHERE idSoiree = :idSoiree";
+        $params = [':idSoiree' => $idSoiree];
+        return $this->prepareAndExecute($sql, $params);
     }
 
     /**
      * Ajoute une nouvelle soirée dans la base de données.
-     * @param mixed $nom
-     * @param mixed $nbPlace
-     * @param mixed $lieu
-     * @param mixed $dateSoiree
-     * @param mixed $heureSoiree
-     * @param mixed $placeAssise
-     * @param mixed $infoComp
+     * @param string $nom
+     * @param int $nbPlace
+     * @param string $lieu
+     * @param string $dateSoiree
+     * @param string $heureSoiree
+     * @param int $placeAssise
+     * @param string $infoComp
      * @return bool|int
      */
-    public function AjouterUneSoiree($nom,$nbPlace,$lieu,$dateSoiree,$heureSoiree,$placeAssise,$infoComp) {
-        $resultatRequete= $this ->exec("INSERT INTO `soiree`( `nom`, `nbPlaces`, `lieu` , `dateSoiree`, `heureSoiree`, `placeAssise`, `infoComp` ) VALUES ('$nom','$nbPlace','$lieu','$dateSoiree','$heureSoiree','$placeAssise','$infoComp')");
-        return $resultatRequete;
+    public function AjouterUneSoiree($nom, $nbPlace, $lieu, $dateSoiree, $heureSoiree, $placeAssise, $infoComp) {
+        $sql = "INSERT INTO soiree (nom, nbPlaces, lieu, dateSoiree, heureSoiree, placeAssise, infoComp)
+                VALUES (:nom, :nbPlaces, :lieu, :dateSoiree, :heureSoiree, :placeAssise, :infoComp)";
+        $params = [
+            ':nom' => $nom,
+            ':nbPlaces' => $nbPlace,
+            ':lieu' => $lieu,
+            ':dateSoiree' => $dateSoiree,
+            ':heureSoiree' => $heureSoiree,
+            ':placeAssise' => $placeAssise,
+            ':infoComp' => $infoComp
+        ];
+        return $this->prepareAndExecute($sql, $params);
     }
 
     /**
      * Modifie les informations d'une soirée existante dans la base de données.
-     * @param mixed $idSoiree
-     * @param mixed $nom
-     * @param mixed $nbPlace
-     * @param mixed $lieu
-     * @param mixed $dateSoiree
-     * @param mixed $heureSoiree
-     * @param mixed $placeAssise
-     * @param mixed $infoComp
+     * @param int $idSoiree
+     * @param string $nom
+     * @param int $nbPlace
+     * @param string $lieu
+     * @param string $dateSoiree
+     * @param string $heureSoiree
+     * @param int $placeAssise
+     * @param string $infoComp
      * @return bool|int
      */
     public function ModifierUneSoiree($idSoiree, $nom, $nbPlace, $lieu, $dateSoiree, $heureSoiree, $placeAssise, $infoComp) {
-        $resultatRequete = $this->exec("UPDATE `soiree` 
-                                        SET `nom` = '$nom', 
-                                            `nbPlaces` = '$nbPlace', 
-                                            `lieu` = '$lieu', 
-                                            `dateSoiree` = '$dateSoiree', 
-                                            `heureSoiree` = '$heureSoiree', 
-                                            `placeAssise` = '$placeAssise', 
-                                            `infoComp` = '$infoComp'  
-                                        WHERE `idSoiree` = $idSoiree");
-        return $resultatRequete;
+        $sql = "UPDATE soiree
+                SET nom = :nom,
+                    nbPlaces = :nbPlaces,
+                    lieu = :lieu,
+                    dateSoiree = :dateSoiree,
+                    heureSoiree = :heureSoiree,
+                    placeAssise = :placeAssise,
+                    infoComp = :infoComp
+                WHERE idSoiree = :idSoiree";
+        $params = [
+            ':idSoiree' => $idSoiree,
+            ':nom' => $nom,
+            ':nbPlaces' => $nbPlace,
+            ':lieu' => $lieu,
+            ':dateSoiree' => $dateSoiree,
+            ':heureSoiree' => $heureSoiree,
+            ':placeAssise' => $placeAssise,
+            ':infoComp' => $infoComp
+        ];
+        return $this->prepareAndExecute($sql, $params);
     }
 }
